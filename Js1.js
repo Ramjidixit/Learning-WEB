a55769f03fc8dbf551289526b23ddbe2da130e52
Ram Dixit <dixitramji108@gmail.com>
2023-11-07 12:28:47 +0530

    learn object

---
+++ Js1.js
@@ -0,0 +1,22 @@
+console.log("Ram Ram");
+
+//object creation
+let rectangle= {
+    length:1,
+    breadth:1
+};
+
+let student={
+    name:"Raju",
+    Marks:86
+}
+
+//factory method of object creation
+
+function createRectangle(){
+    return rectangle= {
+        length:1,
+        breadth:1
+    };
+    
+}
